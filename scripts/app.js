680cd0bbe068a6fbc9b4cc87450628d4eef05cf7
nkcelll <kartozianikoloz1@gmail.com>
2024-08-15 04:34:36 +0400

    Wrote JavaScript

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,51 @@
-const incrElement = document.querySelector('.js-btn-incr')
-const resetElemet = document.querySelector('.js-btn-reset')
-const decrElement = document.querySelector('.js-btn-decr')
-
-
 function display() {
-
     let generatingHTML = '';
+    let counter = 0
+    
 
-    let html = `
+    function updateCounter(){
+        document.querySelector('.js-number-display').innerText = counter
+    }
+    
+
+    function calculate() {
+        const incrElement = document.querySelector('.js-btn-incr')
+        const resetElemet = document.querySelector('.js-btn-reset')
+        const decrElement = document.querySelector('.js-btn-decr')
+
+        incrElement.addEventListener('click', () => {
+            counter++;
+            updateCounter()
+        });
+        resetElemet.addEventListener('click', () => {
+            counter = 0;
+            updateCounter()
+        });
+        decrElement.addEventListener('click', () => {
+            counter--;
+           updateCounter()
+        });
+        
+    };
+
+    generatingHTML += `
         <h1 class="text">Counter</h1>
-            <h1 class="number js-number-display">3</h1>
-            <div class="footer-cont">
+        <h1 class="number js-number-display">${counter}</h1>
+        <div class="footer-cont">
                 <button class="btn-display js-btn-decr">Decrease</button>
                 <button class="btn-display js-btn-reset">Reset</button>
                 <button class="btn-display js-btn-incr">Increase</button>
-            </div> 
+        </div> 
     `;
 
-    generatingHTML += html
+    document.querySelector('.js-display').innerHTML = generatingHTML;
 
-    document.querySelector('.js-display').innerHTML = generatingHTML
+    calculate()
+    
+    updateCounter()
+
 }
-
 display()
+
+
+
